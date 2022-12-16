d8bd38e588cbce12da66095f8dbd5fbc915b6b38
vinoja98 <vinoja.18@cse.mrt.ac.lk>
2022-12-16 11:12:18 +0530

    Initial data parameter added

--- component/src/test/java/io/siddhi/extension/io/live/source/TestCaseOfLiveSource.java
+++ component/src/test/java/io/siddhi/extension/io/live/source/TestCaseOfLiveSource.java
@@ -1,5 +1,8 @@
 package io.siddhi.extension.io.live.source;
 
+// import com.c8db.C8Cursor;
+// import com.c8db.C8DB;
+// import com.c8db.entity.BaseDocument;
 import io.siddhi.core.SiddhiAppRuntime;
 import io.siddhi.core.SiddhiManager;
 import io.siddhi.core.event.Event;
@@ -36,11 +39,12 @@ public class TestCaseOfLiveSource {
      */
     @Test
     public void liveSelect() throws Exception {
+//        List<String> receivedEventNameList = new ArrayList<>(2);
         PersistenceStore persistenceStore = new InMemoryPersistenceStore();
         SiddhiManager siddhiManager = new SiddhiManager();
         siddhiManager.setPersistenceStore(persistenceStore);
         String inStreamDefinition0 = "@App:name('TestSiddhiApp0')" +
-                "@source(type='live',sql.query='FOR t IN NetworkTrafficTable COLLECT browser = t.browser WITH COUNT INTO value RETURN {browser: browser,totalCount: value}', " +
+                "@source(type='live',sql.query='FOR t IN NetworkTrafficTable SORT t.traffic DESC LIMIT 5 RETURN t', " +
                 "host.name='api-peamouth-0b57f3c7.paas.macrometa.io'," +
                 "api.key = 'Tu_TZ0W2cR92-sr1j-l7ACA.newone.9pej9tihskpx2vYZaxubGW3sFCJLzxe55NRh7T0uk1JMYiRmHdiQsWh5JhRXXT6c418385', " +
                 " @map(type='json', fail.on.missing.attribute='false') )" +
