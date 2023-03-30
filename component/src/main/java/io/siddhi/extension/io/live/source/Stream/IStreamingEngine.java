17e5d571dab180038fb5a51d02b4b918adc8f25c
Mahesh <58337761+MaheshMadushan@users.noreply.github.com>
2023-03-30 12:45:45 +0530

    Stream Engine access interface subscribe on demand, no need of adding topic. topic should be provided to the client as creating the client
    Signed-off-by: Mahesh <58337761+MaheshMadushan@users.noreply.github.com>

--- component/src/main/java/io/siddhi/extension/io/live/source/Stream/IStreamingEngine.java
+++ component/src/main/java/io/siddhi/extension/io/live/source/Stream/IStreamingEngine.java
@@ -7,6 +7,6 @@ import java.util.function.Function;
 // according to user preference
 public interface IStreamingEngine<T> {
     void consumeMessage(java.util.function.Consumer<T> consumer); // this method will use by stream thread to consume message (strategy design pattern execute method)
-    void subscribe(String topicOfStream); // this method will use by stream thread to subscribe to topic (strategy design pattern execute method)
+    void subscribe(); // this method will use by stream thread to subscribe to topic (strategy design pattern execute method)
     void unsubscribe(); // this method will use by stream thread to unsubscribe from topic (strategy design pattern execute method)
 }
