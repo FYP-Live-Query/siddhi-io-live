b78f8f7753d7eef9ae3c4593c99da02cb4dd7f65
Mahesh <58337761+MaheshMadushan@users.noreply.github.com>
2023-04-16 07:21:12 +0530

    Consumer cient with non-blocking message handling.
    Signed-off-by: Mahesh <58337761+MaheshMadushan@users.noreply.github.com>

--- component/src/main/java/io/siddhi/extension/io/live/source/Stream/KafkaClient/KafkaConsumerClient.java
+++ component/src/main/java/io/siddhi/extension/io/live/source/Stream/KafkaClient/KafkaConsumerClient.java
@@ -1,6 +1,7 @@
 package io.siddhi.extension.io.live.source.Stream.KafkaClient;
 
 import io.siddhi.extension.io.live.source.Stream.IStreamingEngine;
+import io.siddhi.extension.io.live.source.Stream.KafkaClient.ActiveConsumerRecodHandling.ActiveConsumerRecordHandler;
 import lombok.Builder;
 import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.common.errors.WakeupException;
@@ -22,31 +23,20 @@ public class KafkaConsumerClient<KeyType,ValueType> implements IStreamingEngine<
     private String client_id_config;
     private String topic;
     private final AtomicBoolean waitingInterrupted = new AtomicBoolean(false);
+    private final ActiveConsumerRecordHandler<KeyType,ValueType> activeConsumerRecordHandler
+            = new ActiveConsumerRecordHandler<>();
 
     private final Object lock = new Object();
 
 
     @Override
     public void consumeMessage(java.util.function.Consumer<ValueType> consumer) {
+        activeConsumerRecordHandler.setConsumer(consumer);
+        activeConsumerRecordHandler.start();
         synchronized(lock) {
             try{
                 ConsumerRecords<KeyType, ValueType> consumerRecords = kafkaConsumer.poll(Duration.ofMillis(1000));
-                for (ConsumerRecord<KeyType, ValueType> consumerRecord : consumerRecords) {
-
-                    // TODO :  this should encapsulate (duplicate code)
-                    String stringJsonMsg = consumerRecord.value().toString();
-
-                    JSONObject jsonObject = new JSONObject(stringJsonMsg);
-                    JSONObject newValue = (JSONObject) ((JSONObject) jsonObject.get("payload")).get("after");
-
-                    newValue.put("initial_data", "false"); // as required by the backend processing
-
-                    JSONObject obj = new JSONObject();
-                    obj.put("properties", newValue); // all user required data for siddhi processing inside properties section in JSON object
-                    String strMsg = obj.toString();
-
-                    consumer.accept((ValueType) strMsg); // The Java Consumer interface is a functional interface that represents an function that consumes a value without returning any value.
-                }
+                activeConsumerRecordHandler.addConsumerRecords(consumerRecords);
                 kafkaConsumer.commitSync();
             } catch (WakeupException e){
                 if(!waitingInterrupted.get()) {
